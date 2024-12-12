b9447f1c18a62018b81538376e89e14e1af4e7dc
AbdullahRFA <shakibrybmn@gmail.com>
2024-12-12 13:12:14 +0600

    modified with added admin function

--- Hello_Flask/app.py
+++ Hello_Flask/app.py
@@ -2,8 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/hello")
+@app.route("/")
 def hello_there():
     return "Hello Flask"
+@app.route("/admin")
+def admin():
+    return "This Abdullah Nazmus-Sakib"
 if __name__ == "__main__":
     app.run(debug=True)
